5a0a441b87e5df9edc19f720f8d35e5bd6ffb8a6
Michael Pereira Rocha <michaelrocha9722@gmail.com>
2025-07-24 15:48:14 -0300

    feat: add header on mobile

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+let headerMenu = document.querySelector('.header__menu');
+let headerList = document.querySelector('.header__list');
+headerMenu.addEventListener("click",()=>{
+    headerMenu.classList.toggle("header__menu-active");
+    headerList.classList.toggle("header__list-active");
+})  

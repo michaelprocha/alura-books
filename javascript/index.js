9e00002823a82b13739b5b86dbe970a10d82c6c1
Michael Pereira Rocha <michaelrocha9722@gmail.com>
2025-07-31 17:21:12 -0300

    feat: add newsletter, footer and wornking on header(tablet)

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,8 @@
 let headerMenu = document.querySelector(".header__menu");
 let headerList = document.querySelector(".header__list");
+let headerCategories = document.querySelector(".header__categories");
 headerMenu.addEventListener("click", () => {
 	headerMenu.classList.toggle("header__menu-active");
 	headerList.classList.toggle("header__list-active");
+	headerCategories.classList.toggle("header__categories-active");
 });
